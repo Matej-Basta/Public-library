438bf61c8b20d0166dd53c17faf1fd8b142a3c97
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-29 19:45:24 +0100

    Prepared array with three books

---
+++ script.js
@@ -0,0 +1,24 @@
+const books = [
+  {
+    title: "The Catcher in the Rye",
+    authors: ["J.D. Sallinger"],
+    pages: 234,
+    published: 1951,
+    borrowed: false,
+  },
+
+  {
+    title: "The Godfather",
+    authors: ["Mario Puzo"],
+    pages: 631,
+    published: 1969,
+    borrowed: true,
+  },
+  {
+    title: "1984",
+    authors: ["George Orwell"],
+    pages: 321,
+    published: 1949,
+    borrowed: true,
+  },
+];
